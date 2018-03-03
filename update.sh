eff1c7d0a2097a0ddbc11e2b247c08473e9a5d41
serpent-vlad <kekaadrenalin@hotmail.com>
2018-03-03 13:33:08 +0300

    update auto-updater

--- update.sh
+++ update.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/sh
 
-git reset --hard origin/master
-git pull
-composer update
-php yii cache/flush-all
+read -r -p "Stop the service and pull fresh code? (Y/n)" response
+if ! [[ $response =~ ^([nN][oO]|[nN])$ ]]
+then
+	webservice stop
+	echo -e "\nUpdating the code..."
+	git reset --hard origin/master
+    git pull
+    composer update
+    php yii cache/flush-all
+	echo
+	read -r -p "OK to start the service? (Y/n)" response
+	if ! [[ $response =~ ^([nN][oO]|[nN])$ ]]
+	then
+		webservice start
+	fi
+fi
